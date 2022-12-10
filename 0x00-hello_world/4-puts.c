abd99fffc9f0edb44dcd42e15f10c15a5ee40758
udonna-cell <orabuezestanley@gmail.com>
2022-12-10 03:23:47 +0100

    added file 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/*
+ * main - prints texts using puts
+ * Returns: returns 0
+ */
+
+int main(void) {
+	puts("Programming is like building a multilingual puzzle\n");
+	return(0);
+}
