bc7dc8126e7ed00ba6b46c1ade2a498c256d0e26
udonna-cell <orabuezestanley@gmail.com>
2022-12-11 16:22:48 +0100

    fixed file 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+
 /**
  * main - prints the size of data type
  * Return: returns 0
@@ -10,6 +11,6 @@ int main(void)
 	printf("size of int: %d byte(s)\n", sizeof(int));
 	printf("size of long int: %d byte(s)\n", sizeof(unsigned int));
 	printf("size of long long int: %d byte(s)\n", sizeof(long));
-	printf("size of float: %d byte(s)", sizeof(float));
+	printf("size of float: %d byte(s)\n", sizeof(float));
 	return (0);
 }
