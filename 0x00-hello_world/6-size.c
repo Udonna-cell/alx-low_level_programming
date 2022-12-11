d30a5f48da474aafd81bb83c144e8102f9038392
udonna-cell <orabuezestanley@gmail.com>
2022-12-11 13:16:44 +0100

    added file 6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - prints the size of data type
+ * Return: returns 0
+ */
+
+int main(void)
+{
+	printf("size of char: %d byte(s)",sizeof(char));
+       	printf("size of int: %d byte(s)",sizeof(int));
+       	printf("size of float: %d byte(s)",sizeof(float));
+	return (0);
+}
