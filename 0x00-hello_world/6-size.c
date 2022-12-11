379086616a31427e17512bf9f546932e791c42ba
udonna-cell <orabuezestanley@gmail.com>
2022-12-11 13:33:07 +0100

    fixed file 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,7 +7,7 @@
 int main(void)
 {
 	printf("size of char: %d byte(s)", sizeof(char));
-       	printf("size of int: %d byte(s)", sizeof(int));
-       	printf("size of float: %d byte(s)", sizeof(float));
+	printf("size of int: %d byte(s)", sizeof(int));
+	printf("size of float: %d byte(s)", sizeof(float));
 	return (0);
 }
