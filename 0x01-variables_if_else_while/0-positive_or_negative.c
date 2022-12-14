39d1d1dba7412808dc4441f649c460b9de8bce17
udonna-cell <orabuezestanley@gmail.com>
2022-12-14 16:14:10 +0100

    fixed file 0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -6,10 +6,9 @@
  * Return: returns 0
  */
 
- /* more headers goes there */ 
-  
- /* betty style doc for function main goes there */ 
- int main(void)
+/* more headers goes there */
+/* betty style doc for function main goes there */
+int main(void)
  {
 	 int n;
 	 srand(time(0));
@@ -27,5 +26,5 @@
 	 {
 		 printf("%d is zero", n);
 	 }
-	 return (0); 
+	 return (0);
  }
